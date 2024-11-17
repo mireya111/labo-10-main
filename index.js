2363fc15a9683b3a925cfc8c6f0e2504f77a0cb9
PaulCabreaEPN <paul.cabrera@epn.edu.ec>
2024-11-17 10:23:24 -0500

    init

---
+++ index.js
@@ -0,0 +1,57 @@
+//Importar express - COMMONJS
+import express from 'express';
+
+//Inicializar express
+const app = express ()
+
+//RUTA
+//Integrantes
+app.get('/',(req, res)=>{
+    res.json([
+        {
+            id:1,
+            nombre:"Paul",
+            apellido:"Cabrera",
+            edad:20,
+            carrera:"Desarrollo de Software"
+        },
+        {
+            id:2,
+            nombre:"Mireya",
+            apellido:"García",
+            edad:20,
+            carrera:"Desarrollo de Software"
+        },
+        {
+            id:3,
+            nombre:"Mateo",
+            apellido:"Torres",
+            edad:20,
+            carrera:"Desarrollo de Software"
+        },
+        {
+            id:4,
+            nombre:"Ariel",
+            apellido:"Catucuamba",
+            edad:20,
+            carrera:"Desarrollo de Software"
+        },
+        {
+            id:5,
+            nombre:"Matias",
+            apellido:"Terán",
+            edad:21,
+            carrera:"Desarrollo de Software"
+        },
+        {
+            id:6,
+            nombre:"Anthony",
+            apellido:"Astudillo",
+            edad:21,
+            carrera:"Desarrollo de Software"
+        }
+    ])
+});
+
+app.listen(3000);
+console.log("Server OK");
